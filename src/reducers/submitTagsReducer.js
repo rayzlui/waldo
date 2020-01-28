521d3877c28d9ac5d57ea6c9aa138e40cf75d6db
Ray <rayzlui@gmail.com>
2020-01-27 21:42:03 -0800

    submit tags reducer

---
+++ src/reducers/submitTagsReducer.js
@@ -0,0 +1,12 @@
+import { SUBMIT_TAGS_ERROR, SUBMIT_TAGS_SUCCESS } from '../actions/actionTypes';
+
+export function submitTagReducer(action) {
+  switch (action.type) {
+    case SUBMIT_TAGS_ERROR:
+      return false;
+    case SUBMIT_TAGS_SUCCESS:
+      return true;
+    default:
+      return null;
+  }
+}
