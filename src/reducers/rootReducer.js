f4c059e92231fd4ca047b5b7890bfc9c7828c05a
Ray <rayzlui@gmail.com>
2020-01-28 23:28:03 -0800

    pulled previous useState hooks into redux

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,19 @@ import { combineReducers } from 'redux';
 import { gameTagsReducer } from './gameTagsReducer';
 import { imageIndexReducer } from './imageIndexReducer';
 import { submitTagsReducer } from './submitTagsReducer';
+import { currentImageReducer } from './currentImageReducer';
+import { isTagModeReducer } from './isTagModeReducer';
+import { gameScoreReducer } from './gameScoreReducer';
+import { networkErrorReducer } from './networkErrorReducer';
+import { currentGridReducer } from './currentGridReducer';
 
 export const rootReducer = combineReducers({
+  error: networkErrorReducer,
   index: imageIndexReducer,
   tags: gameTagsReducer,
   submitError: submitTagsReducer,
+  currentImage: currentImageReducer,
+  isTagMode: isTagModeReducer,
+  gameScore: gameScoreReducer,
+  currentGrid: currentGridReducer,
 });
