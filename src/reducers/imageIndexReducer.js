5949ae3d3eae837bec277ab78aeb571c6ffbd943
Ray <rayzlui@gmail.com>
2020-01-29 23:34:56 -0800

    add additional start info to handle incase of server outage

--- src/reducers/imageIndexReducer.js
+++ src/reducers/imageIndexReducer.js
@@ -1,7 +1,19 @@
 import { FETCH_IMAGE_SUCCESS } from '../actions/actionTypes';
 
 const initialState = [
-  'https://www.muraldecal.com/en/img/fomi030_1-jpg/folder/products-detalle-muestras-grandes/wall-murals-minions.jpg',
+  {
+    tags: [
+      ['5e2b8a7f053daa085dfd6904', '17'],
+      ['5cb6c63f4a885951900ae0fd', '19'],
+      ['5e2b8a7f053daa085dfd6905', '22'],
+      ['5e2b8a7f053daa085dfd6906', '34'],
+    ],
+    _id: '5cb6c6094f41f3516e2fc4f4',
+    key: 2,
+    photo:
+      'https://www.muraldecal.com/en/img/fomi030_1-jpg/folder/products-detalle-muestras-grandes/wall-murals-minions.jpg',
+    __v: 2,
+  },
 ];
 
 export function imageIndexReducer(state = initialState, action) {
