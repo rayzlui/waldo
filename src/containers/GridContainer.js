afc2035b51f6f1bf8b981f6f905676026cd4e8f1
Ray <rayzlui@gmail.com>
2020-01-29 23:39:00 -0800

    pass props to Grid view

---
+++ src/containers/GridContainer.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import Grid from '../views/Grid';
+import { changeGrid, resetGrid } from '../actions/actions';
+
+function mapStateToProps(state) {
+  return {
+    currentImage: state.currentImage,
+    currentGrid: state.currentGrid,
+    tags: state.tags,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    changeGrid: gridId => dispatch(changeGrid(gridId)),
+    resetGrid: () => dispatch(resetGrid()),
+  };
+}
+
+export const GridContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Grid);
