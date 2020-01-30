4ad95d40da768c103c549875987af3a1fdd9d8e4
Ray <rayzlui@gmail.com>
2020-01-29 23:35:57 -0800

    organize json data into more easily usuable hash map

--- src/reducers/gameTagsReducer.js
+++ src/reducers/gameTagsReducer.js
@@ -3,7 +3,13 @@ import { FETCH_TAG_SUCCESS } from '../actions/actionTypes';
 export function gameTagsReducer(state = {}, action) {
   switch (action.type) {
     case FETCH_TAG_SUCCESS:
-      return action.tags;
+      let organizedData = action.tags.reduce((acc, curr) => {
+        let key = curr._id;
+        let name = curr.tag;
+        acc[key] = name;
+        return acc;
+      }, {});
+      return organizedData;
     default:
       return state;
   }
