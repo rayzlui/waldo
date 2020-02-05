d7509376b7e175951e0bfaba692f154a307e66c2
Ray <rayzlui@gmail.com>
2020-02-04 22:33:32 -0800

    add provider for app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from './configureStore';
-import { RootView } from './views/RootView';
 import './App.scss';
+import { RootContainer } from './containers/RootContainer';
 
 export default function App() {
   let store = configureStore();
   return (
     <Provider store={store}>
-      <RootView />
+      <RootContainer />
     </Provider>
   );
 }
