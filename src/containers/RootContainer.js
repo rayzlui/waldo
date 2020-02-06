7801a961e04c5f7aa13b1a8f67d0fa3ec2a8f83b
Ray <rayzlui@gmail.com>
2020-02-05 22:52:27 -0800

    add props and functions to handle tags view

--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -1,14 +1,33 @@
-import { resetGrid } from '../actions/actions';
+import {
+  resetGrid,
+  viewTags,
+  resetTagSearch,
+  viewImage,
+} from '../actions/actions';
 import { connect } from 'react-redux';
 import { RootView } from '../views/RootView';
+
+function mapStateToProps(state) {
+  return {
+    isViewingTags: state.isViewingTags,
+  };
+}
 
 function mapDispatchToProps(dispatch) {
   return {
     resetGrid: () => dispatch(resetGrid()),
+    viewAllTags: isViewing => {
+      if (!isViewing) {
+        dispatch(viewTags());
+        dispatch(resetTagSearch());
+      } else {
+        dispatch(viewImage());
+      }
+    },
   };
 }
 
 export const RootContainer = connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(RootView);
