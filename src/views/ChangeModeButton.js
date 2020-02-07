7ffa9347dc8c7c9aee26ce70ea37309421871513
Ray <rayzlui@gmail.com>
2020-02-06 22:52:36 -0800

    add button to change between tags vs image

--- src/views/ChangeModeButton.js
+++ src/views/ChangeModeButton.js
@@ -22,3 +22,17 @@ ChangeToTagButton.propTypes = {
   isTagMode: PropTypes.bool,
   changeGameMode: PropTypes.func,
 };
+
+export function ViewTagsButton(props) {
+  const { isViewingTags, viewAllTags } = props;
+  return (
+    <button onClick={() => viewAllTags(isViewingTags)}>
+      {!isViewingTags ? 'View Tags' : 'View Images'}
+    </button>
+  );
+}
+
+ViewTagsButton.propTypes = {
+  isViewingTags: PropTypes.bool,
+  viewAllTags: PropTypes.func,
+};
