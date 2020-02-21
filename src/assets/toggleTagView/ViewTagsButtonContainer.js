93dc6b42d26bc92daa2c7698dbe317244c42c978
Ray <rayzlui@gmail.com>
2020-02-20 21:46:46 -0800

    organize assets, change action dispatch to object

---
+++ src/assets/toggleTagView/ViewTagsButtonContainer.js
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import { ViewTagsButton } from '../changeButton/ChangeModeButton';
+import { viewTags, resetTagSearch, viewImage } from '../../actions/actions';
+
+function mapStateToProps(state) {
+  return {
+    isViewingTags: state.isViewingTags,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    viewAllTags: isViewing => {
+      if (!isViewing) {
+        dispatch(viewTags());
+        dispatch(resetTagSearch());
+      } else {
+        dispatch(viewImage());
+      }
+    },
+  };
+}
+
+export const ViewTagsButtonContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ViewTagsButton);
