28edf2e4bb9f290bdec87908f35b2eb3928c1052
Ray <rayzlui@gmail.com>
2020-02-21 22:06:40 -0800

    update tag links to be object instead of array

--- waldo_server/taggingmodel.js
+++ waldo_server/taggingmodel.js
@@ -3,6 +3,6 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let tagSchema = new Schema({
   tag: String,
-  photo_id: Array,
+  photo_id: Object,
 });
 module.exports = mongoose.model('Taggings', tagSchema);
