2eaa8cdfae01bc52b561f86681888aaaa98dbb38
Ray <rayzlui@gmail.com>
2020-02-24 23:12:25 -0800

    photo_id to map

--- waldo_server/taggingmodel.js
+++ waldo_server/taggingmodel.js
@@ -3,6 +3,6 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let tagSchema = new Schema({
   tag: String,
-  photo_id: Object,
+  photo_id: Map,
 });
 module.exports = mongoose.model('Taggings', tagSchema);
