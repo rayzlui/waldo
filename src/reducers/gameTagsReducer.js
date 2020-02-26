4c36c517dceaf703531223b9cdb95ba394e49131
Ray <rayzlui@gmail.com>
2020-02-25 21:40:09 -0800

    update reducer to handle new schema

--- src/reducers/gameTagsReducer.js
+++ src/reducers/gameTagsReducer.js
@@ -3,13 +3,10 @@ import { FETCH_TAG_SUCCESS } from '../actions/actionTypes';
 export function gameTagsReducer(state = {}, action) {
   switch (action.type) {
     case FETCH_TAG_SUCCESS:
-      let organizedData = action.tags.reduce((acc, curr) => {
-        let key = curr._id;
-        let name = curr.tag;
-        acc[key] = name;
+      return action.tags.reduce((acc, curr) => {
+        acc[curr._id] = curr;
         return acc;
       }, {});
-      return organizedData;
     default:
       return state;
   }
