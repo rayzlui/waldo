a68afb95896f16be1a2712d9768fa80213f888e3
Ray <rayzlui@gmail.com>
2020-02-26 22:37:26 -0800

    input function handles updating current image with new tag

--- src/assets/inputBar/InputBarView.js
+++ src/assets/inputBar/InputBarView.js
@@ -2,17 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export function InputBox(props) {
-  let { confirmSubmit, currentImage, gridId, isTagMode } = props;
+  let { confirmSubmit, currentImage, gridId, isTagMode, overrideTag } = props;
   if (!isTagMode) return null;
   if (!currentImage) return null;
   let imageId = currentImage.key;
   function submitTags(event) {
     let key = event.keyCode || event.which;
     if (key === 13) {
-      //this is essentially the submit for newtag.
       let value = event.target.value;
       let saveTag = window.confirm('Save tag to image?');
       if (saveTag) {
+        let tagId = currentImage.tags[gridId];
+        let imageKey = currentImage.key;
+        if (tagId) {
+          let override = window.confirm(
+            'There is already a tag here, would you like to override?',
+          );
+          if (!override) {
+            return null;
+          }
+          overrideTag(tagId, imageKey, gridId);
+        }
         confirmSubmit(imageId, value, gridId);
       }
     }
@@ -31,4 +41,5 @@ InputBox.propTypes = {
   confirmSubmit: PropTypes.func,
   currentImage: PropTypes.object,
   gridId: PropTypes.number,
+  overrideTag: PropTypes.func,
 };
