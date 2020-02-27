208051ccd7c415deadebbb867fb4acc91ffb9c68
Ray <rayzlui@gmail.com>
2020-02-26 22:37:10 -0800

    input function handles updating current image with new tag

--- src/assets/inputBar/InputBarContainer.js
+++ src/assets/inputBar/InputBarContainer.js
@@ -2,9 +2,8 @@ import { connect } from 'react-redux';
 import {
   postTags,
   resetGrid,
-  retrieveImageIndex,
-  retrieveAllTags,
   resetCurrentImage,
+  overrideTags,
 } from '../../actions/actions';
 import { InputBox } from './InputBarView';
 
@@ -19,12 +18,13 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
   return {
+    //it's not done saving by the time the fetches are called henceforthwith, it's not showing up in the image data, but it does show in tags hm.
     confirmSubmit: (imageId, value, gridId) => {
       dispatch(postTags({ imageId, value, gridId }));
-      dispatch(retrieveImageIndex());
-      dispatch(retrieveAllTags());
-      dispatch(resetCurrentImage(imageId));
       dispatch(resetGrid());
+    },
+    overrideTag: (tagId, imageKey, gridId) => {
+      dispatch(overrideTags(tagId, imageKey, gridId));
     },
   };
 }
