e4b0e15e1a224673a21203841a22d8b86124ff4b
Ray <rayzlui@gmail.com>
2020-02-27 22:28:10 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from './configureStore';
 import './App.scss';
-import { RootContainer } from './containers/RootContainer';
+import { RootContainer } from './assets/root/RootContainer';
 
 export default function App() {
   let store = configureStore();
