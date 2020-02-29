33716c201c068cc9a0adcb8dcfdc34eb31fca24e
Ray <rayzlui@gmail.com>
2020-02-28 23:28:19 -0800

    move image related containers into image view

--- src/assets/root/ImageViewDisplay.js
+++ src/assets/root/ImageViewDisplay.js
@@ -4,14 +4,22 @@ import { ScoreContainer } from '../scoreSection/ScoreContainer';
 import { CurrentImageContainer } from '../currentImage/CurrentImageContainer';
 import { GridDOM } from '../buildGrid/GridDOM';
 import PropTypes from 'prop-types';
+import { EditImageContainer } from '../editImage/EditImageContainer';
+import { DeleteImageContainer } from '../deleteImage/DeleteImageContainer';
+import { ImageIndexContainer } from '../imageIndex/ImageIndexContainer';
+import { NewImageContainer } from '../addNewImage/NewImageContainer';
 
 export function ImageViewDisplay(props) {
   const { isViewingTags } = props;
   if (isViewingTags) return null;
   return (
     <section className="selected__image">
+      <NewImageContainer />
+      <ImageIndexContainer />
       <ScoreHeaderContainer />
       <ScoreContainer />
+      <EditImageContainer />
+      <DeleteImageContainer />
       <GridDOM />
       <CurrentImageContainer />
     </section>
