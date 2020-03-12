03d179563996de04829b156a8dd263d95c3ef2d0
Ray <rayzlui@gmail.com>
2020-03-11 21:35:05 -0700

    savingImageTest

---
+++ src/actions/actions.test.js
@@ -0,0 +1,12 @@
+import { savingImageStart } from "./actions";
+import { SAVING_IMAGE_START } from "./actionTypes";
+
+function basicActionTestHelper(action, actionType) {
+  return describe(`${action.name}`, () => {
+    it(`should return { type: ${actionType} }`, () => {
+      expect(action()).toEqual({ type: actionType });
+    });
+  });
+}
+
+basicActionTestHelper(savingImageStart, SAVING_IMAGE_START)
